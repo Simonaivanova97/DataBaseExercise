a0cf60db520bf0a80e37394cf204bdc879900008
Simonaivanova97 <ivanova.97@abv.bg>
2021-09-02 19:53:53 +0300

    Update ThirthExersiceShips.sql

--- ThirthExersice/ThirthExersiceShips.sql
+++ ThirthExersice/ThirthExersiceShips.sql
@@ -5,14 +5,14 @@ use ships;
 select distinct country
 from classes
 where numguns>=all(select numguns
-					from classes);
+		   from classes);
                     
 /*3.2 . Напишете заявка, която извежда имената на корабите с 16-инчови оръдия
 (bore).*/
 select distinct name
 from ships 
 where class in (select class
-				from classes 
+		from classes 
                 where bore=16);
                 
 /*3.3 Напишете заявка, която извежда имената на битките, в които са участвали 
@@ -20,15 +20,23 @@ where class in (select class
 select distinct battle
 from outcomes
 where ship in (select name
-				from ships
-                where class='Kongo');
+	       from ships
+               where class='Kongo');
                     
 /*3.4 Напишете заявка, която извежда имената на корабите, чиито брой оръдия е 
 най-голям в сравнение с корабите със същия калибър оръдия (bore).*/
+-- 1 начин.
 select s1.name
 from ships s1
 join classes c1 on s1.class=c1.class
 where c1.numguns>=all(select c2.numguns
-					from classes c2
-                    where c1.bore=c2.bore); 
-
+		      from classes c2
+                      where c1.bore=c2.bore); 
+-- 2 начин.
+select name 
+from ships
+where class in (select c1.class 
+		from classes c1
+                where c1.numguns>=all(select numguns
+				      from classes 
+                                      where bore=c1.bore));
