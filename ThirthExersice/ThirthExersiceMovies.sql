8c79b8c8cef5cb984fad46208fbba4134c61770e
Simonaivanova97 <ivanova.97@abv.bg>
2021-09-02 20:05:40 +0300

    Update ThirthExersiceMovies.sql

--- ThirthExersice/ThirthExersiceMovies.sql
+++ ThirthExersice/ThirthExersiceMovies.sql
@@ -5,12 +5,12 @@ use movies;
 select name 
 from moviestar
 where gender='F' and name in(select name 
-							from movieexec
-                            where networth>10000000);
+			     from movieexec
+                             where networth>10000000);
 
 /*1.2 . Напишете заявка, която извежда имената на тези филмови звезди, които не 
 са продуценти.*/
 select name
 from moviestar
 where name not in (select name
-					from movieexec);
+		   from movieexec);
