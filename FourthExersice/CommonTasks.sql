d45339b9549baa0beaabaa0aae36e29066f91d4d
Simonaivanova97 <ivanova.97@abv.bg>
2021-09-03 00:47:08 +0300

    Update CommonTasks.sql

--- FourthExersice/CommonTasks.sql
+++ FourthExersice/CommonTasks.sql
@@ -29,27 +29,27 @@ use ships;
 select name
 from ships
 where name in (select ship
-				from outcomes) and (name like 'C%' or name like 'K%');
+	       from outcomes) and (name like 'C%' or name like 'K%');
 
 /*5. Всички държави, които имат потънали в битка кораби.*/
 select country
 from classes
 where class in (select class 
-				from ships
+		from ships
                 where name in (select ship
-								from outcomes
-								where result='sunk'));
+			       from outcomes
+			       where result='sunk'));
 
 /*6. Всички държави, които нямат нито един потънал кораб.*/
 select distinct country
 from classes
 where country not in (select country
-					from classes
-                    where class in (select class
-									from ships
-                                    where name in (select ship
-													from outcomes
-													where result='sunk')));
+		      from classes
+                      where class in (select class
+				      from ships
+                                      where name in (select ship
+						     from outcomes
+						     where result='sunk')));
 
 /*7. (От държавен изпит) Имената на класовете, за които няма кораб, пуснат на вода
 (launched) след 1921 г. Ако за класа няма пуснат никакъв кораб, той също трябва
@@ -58,8 +58,8 @@ select distinct classes.class
 from classes
 left join ships on classes.class=ships.class and launched<=1921
 where classes.class not in (select class
-							from ships
-							where launched>1921);
+			    from ships
+			    where launched>1921);
 
 /*8. Името, държавата и калибъра (bore) на всички класове кораби с 6, 8 или 10
 оръдия. Калибърът да се изведе в сантиметри (1 инч е приблизително 2.54 см).*/
@@ -72,14 +72,14 @@ where numguns=6 or numguns=8 or numguns=10;
 select distinct c1.country
 from classes c1
 where c1.country in (select c2.country
-					from classes c2
-                    where c1.country=c2.country and c1.bore<>c2.bore and c1.class<>c2.class);
+		     from classes c2
+                     where c1.country=c2.country and c1.bore<>c2.bore and c1.class<>c2.class);
 
 /*10. Страните, които произвеждат кораби с най-голям брой оръдия (numguns).*/
 select distinct country
 from classes
 where numguns >= all (select numguns
-						from classes);
+		      from classes);
   
 use pc;
 /*11. Компютрите, които са по-евтини от всеки лаптоп на същия производител.*/
@@ -87,9 +87,9 @@ select pc.*
 from pc
 join product p1 on pc.model=p1.model
 where price < all (select price
-					from laptop 
-                    join product p2 on laptop.model=p2.model
-                    where p1.maker=p2.maker);
+		   from laptop 
+                   join product p2 on laptop.model=p2.model
+                   where p1.maker=p2.maker);
                     
 /*12. Компютрите, които са по-евтини от всеки лаптоп и принтер на същия 
 производител.*/
@@ -97,12 +97,9 @@ select pc.*
 from pc
 join product p1 on pc.model=p1.model
 where price < all (select price
-					from laptop 
-                    join product p2 on laptop.model=p2.model
-                    where p1.maker=p2.maker) and price < all (select price
-																from printer 
-																join product p3 on printer.model=p3.model
-																where p1.maker=p3.maker);
-					
-                    
-
+		   from laptop 
+                   join product p2 on laptop.model=p2.model
+                   where p1.maker=p2.maker) and price < all (select price
+							     from printer 
+							     join product p3 on printer.model=p3.model
+							     where p1.maker=p3.maker);
