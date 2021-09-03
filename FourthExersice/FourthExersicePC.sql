b444eb1a2ebd7d5a241537a5e361389aa95ef8bc
Simonaivanova97 <ivanova.97@abv.bg>
2021-09-03 11:18:26 +0300

    Delete FourthExersice directory

--- FourthExersice/FourthExersicePC.sql
+++
@@ -1,9 +0,0 @@
-use pc;
-
-/*2.1 Напишете заявка, която извежда производител, модел и тип на продукт за тези 
-производители, за които съответният продукт не се продава (няма го в таблиците PC, 
-Laptop или Printer).*/
-select maker, model, type 
-from product
-where model not in (select model from pc) and model not in (select model from laptop)
-and model not in (select model from printer);
